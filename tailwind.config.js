2a36c9f74766b766d5787385ab8ff4b9cb1746bf
Lane Keck <122398068+lanekooky@users.noreply.github.com>
2024-11-19 19:40:27 +0000

    Fixed html to include style sheet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html',
-    './script.js',
+  content: ['./public/*.{html,js}',
   ],
   theme: {
     extend: {},
